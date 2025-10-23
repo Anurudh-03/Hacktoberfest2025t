8c4630cd9f08c60b92185efb1899da18f0cb68dd
Anurudh-03 <usernotfounded21@gmail.com>
2025-10-24 00:49:28 +0530

    Implement binary search for insert position

---
+++ insert.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static int searchInsert(int [] arr, int x) {
+        int n = arr.length; // size of the array
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
